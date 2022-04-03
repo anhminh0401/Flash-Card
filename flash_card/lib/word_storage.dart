f22043b228a6b42fee128ec15b608b683f88ecec
anhminh <nguyenanhminh04012001@gmail.com>
2022-04-03 18:43:50 +0700

    add list word screen

--- flash_card/lib/word_storage.dart
+++ flash_card/lib/word_storage.dart
@@ -12,28 +12,65 @@ class _WordStorageState extends State<WordStorage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Flash Card")),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          ElevatedButton(
-              onPressed: () {},
-              child: const Icon(
-                Icons.add,
-                size: 50,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            ElevatedButton(
+                onPressed: () {},
+                child: const Icon(
+                  Icons.add,
+                  size: 50,
+                ),
+                style:
+                    ElevatedButton.styleFrom(minimumSize: const Size(90, 70))),
+            ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.search, size: 40),
+                style:
+                    ElevatedButton.styleFrom(minimumSize: const Size(90, 70))),
+          ]),
+          const SizedBox(height: 50),
+          for (Word item in word)
+            Container(
+              color: Colors.lightBlue,
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                      width: 150,
+                      child: Text(
+                        item.word,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                  Text(item.definition,
+                      style: const TextStyle(fontStyle: FontStyle.italic)),
+                ],
               ),
-              style: ElevatedButton.styleFrom(minimumSize: const Size(90, 70))),
-          ElevatedButton(
-              onPressed: () {},
-              child: const Icon(Icons.search, size: 40),
-              style: ElevatedButton.styleFrom(minimumSize: const Size(90, 70))),
+            )
         ]),
-        const SizedBox(
-          height: 50
-        ), 
-      
-      ]),
+      ),
     );
   }
 }
+
+class Word {
+  final String definition;
+  final String word;
+
+  Word({required this.word, required this.definition});
+}
+
+List<Word> word = <Word>[
+  Word(word: "word", definition: "definition"),
+  Word(word: "monster", definition: "quái vật"),
+  Word(word: "time", definition: "thời gian"),
+  Word(word: "word", definition: "definition"),
+  Word(word: "word", definition: "definition"),
+  Word(word: "word", definition: "definition"),
+  Word(word: "word", definition: "definition"),
+  Word(word: "word", definition: "definition"),
+  Word(word: "word", definition: "definition"),
+];
